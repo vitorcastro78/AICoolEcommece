d73a00d76d493d73d5a9055693063f251acbd0d0
Vitor Castro <vitorcastro78@gmail.com>
2025-07-18 21:05:42 +0100

    Add ZippedBeans.Zip.Backend.Application.WebAPI.Models.Customers.GetCustomerResponseTypes component

---
+++ src/types/ZippedBeans.Zip.Backend.Application.WebAPI.Models.Customers.GetCustomerResponseTypes.ts
@@ -0,0 +1,21 @@
+// ZippedBeans.Zip.Backend.Application.WebAPI.Models.Customers.GetCustomerResponse.ts
+
+export interface Address {
+  street: string;
+  city: string;
+  state?: string;
+  postalCode: string;
+  country: string;
+}
+
+export interface GetCustomerResponse {
+  accountNumber: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  currency: string;
+  phoneNumber: string;
+  addresses: Address[];
+  additionalEmails: string[];
+  accountId: string;
+}
